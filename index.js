1462a8acc616404c2a3898b84436669b4e75bf79
Pronepoet <ochiengphillip4@gmail.com>
2022-08-17 01:44:56 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+const num1 = 2
+const num2 = 31
+const multiply = num1*num2
+
+const random = Math.round(Math.random()+1);
+const num3 = 14
+const num4 = 10
+const mod = num3%num4;
+const max = Math.max(1,2,3,4,20)
